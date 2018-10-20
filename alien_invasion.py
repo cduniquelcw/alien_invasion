93d963e132d099df701163950103e1614450ab4f
So <zhuxuaaa@qq.com>
2018-10-21 02:16:21 +0800

    10-21

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 
 
 from settings import Settings
@@ -16,6 +17,9 @@ def run_game():
 	ai_settings=Settings()
 	screen=pygame.display.set_mode((ai_settings.screen_wh))
 	pygame.display.set_caption('Alien Invasion')
+
+	#创建Play按钮
+	play_button = Button(ai_settings,screen,'Play')
 
 	#创建一个用于存储游戏统计信息的实例
 	stats = GameStats(ai_settings)
@@ -32,13 +36,13 @@ def run_game():
 
 	#开始主循环
 	while True:
-		gf.check_events(ai_settings,screen,ship,bullets)
+		gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
 
 		if stats.game_active:
 			ship.update()
 			gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
 			gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
 
-		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+		gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 
 run_game()
